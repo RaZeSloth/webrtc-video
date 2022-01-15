3aaec536bedc8116ccc489dfbce2dfd8268f1c80
RaZeSloth <razeslothc@gmail.com>
2022-01-15 11:22:02 +0200

    Essa commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# WebRTC Videosilla loovtöö
+
+
+`yarn` - Et installeerida kõik packagid
+`yarn start` - Et alustada
+
+[info](https://www.youtube.com/watch?v=IqPJb6o_S1Q)
+[Artikkel](https://cloudnweb.dev/2019/09/building-p2p-video-chat-application-using-webrtc-and-node-js/)
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import Player from './Utils/player';
+import signalhub from "signalhub";
+import createSwarm from "webrtc-swarm";
+
+navigator.mediaDevices
+  .getUserMedia({ /* video: true, */ audio: true })
+  .then(function (stream) {
+
+    const hub = signalhub("my-game", [ "http://localhost:8080" ])
+    const swarm = createSwarm(hub, {
+      stream: stream,
+    })
+
+    const you = new Player({ x: 0, y: 0, color: "black", left: 0, top: 0 })
+    you.addStream(stream)
+    const players = {}
+    swarm.on("connect", function (peer, id) {
+      if (!players[ id ]) {
+        players[ id ] = new Player({
+          x: 300,
+          y: 0,
+          left: 200,
+          top: 0,
+          color: "red",
+        })
+        peer.on("data", function (data) {
+          data = JSON.parse(data.toString())
+          players[ id ].update(data)
+        })
+        players[ id ].addStream(peer.stream)
+      }
+    })
+
+    swarm.on("disconnect", function (peer, id) {
+      if (players[ id ]) {
+        players[ id ].element.parentNode.removeChild(players[ id ].element)
+        delete players[ id ]
+      }
+    })
+    setInterval(function () {
+      console.log("Interval Call")
+      you.update()
+      const youString = JSON.stringify(you)
+      swarm.peers.forEach(function (peer) {
+        peer.send(youString)
+      })
+    }, 100)
+    document.addEventListener('keypress', function (e) {
+      const speed = 16
+      switch (e.key) {
+        case 'a':
+          you.x -= speed
+          break
+        case 'd':
+          you.x += speed
+          break
+        case 'w':
+          you.y -= speed
+          break
+        case 's':
+          you.y += speed
+          break
+      }
+    }, false)
+
+})
+
