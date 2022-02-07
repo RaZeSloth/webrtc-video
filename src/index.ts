d99da10ff211e04e5caa80ec54654abbd85011c0
RaZe <razeslothc@gmail.com>
2022-02-07 11:21:48 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import uusBrauseriÜhendus from './Utils/player';
 import signalhub from "signalhub";
 import createSwarm from "webrtc-swarm";
-import fs from "node:fs";
+import fs from "fs";
 
 navigator.mediaDevices
   .getUserMedia({ video: true, audio: true })
   .then((stream: MediaStream) => {
 
-    const hub = signalhub("webrtc-connection", [ `http://localhost:${fs.readFileSync("./port.txt").toString() || 3000}` ])
+    const hub = signalhub("webrtc-connection", [ `http://localhost:${parseInt(fs.readFileSync("./port.txt").toString()) || 3000}` ])
     const swarm = createSwarm(hub, {
       stream: stream,
     })
